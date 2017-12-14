3c6267c144d6393c1c98e37cfef3def996f11906
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2017-12-14 15:59:15 +0100

    Improvements to JS

--- designs.js
+++ designs.js
@@ -1,19 +1,35 @@
 //click signUp-btn
-    //click logIn-btn
-    //Hide logIn-box or signUp-box 
+//click logIn-btn
+//Hide logIn-box or signUp-box 
 
-$(document).ready(function() {      
+$(document).ready(function() { 
+    $(".signUp-box").hide();
+    $(".hello").hide();
+    $(".logIn-btn").addClass("active-button");
+
     $(".signUp-btn").click(function() {
         $(".logIn-box").hide();
         $(".signUp-box").show();
         $(".signUp-btn").addClass("active-button");
+        $(".logIn-btn").removeClass("active-button");
     })
-})
 
-$(document).ready(function() {      
     $(".logIn-btn").click(function() {
         $(".signUp-box").hide();
         $(".logIn-box").show();
         $(".logIn-btn").addClass("active-button");
+        $(".signUp-btn").removeClass("active-button");
     })
+
+    $("input").blur(function(){
+        if($(this).val() == ''){
+            $(".button-big").click(function() {
+                $(".container").hide();
+                $(".hello").show();
+            })
+        }
+    });
+    
 })
+
+
